b35a6e804d04f38c80ddbf80eb3d22e885d8a1f4
Mohammad Javad Taleb <mohammadjavadtaleb@Mohammads-MacBook-Pro.local>
2023-06-06 13:00:41 +0330

    added some classes in chat page

--- src/pages/admin/tickets/answer/[ticketId].js
+++ src/pages/admin/tickets/answer/[ticketId].js
@@ -6,8 +6,62 @@ export default function Answer() {
 
     return (
         <>
-            <div className={"container"}>
-                <div className={"d-flex flex-column gap-3"}>
+            <div className={"container mb-5"}>
+                <div className={"d-flex flex-column gap-3 mb-5"}>
+                    <div className={"d-flex flex-row gap-2"}>
+                        <Tooltip title={"پشتیبانی وبسایت"}>
+                            <div className={"company-profile-img align-self-end"}>
+                                <img alt={""} src={"/img/consulting.svg"} className={"w-100"}/>
+                            </div>
+                        </Tooltip>
+                        <div className={"chat-item text-justify bg-white rounded-3 py-3 px-3 shadow-sm"}>
+                            سلام چطور میتونم کمکتون کنم ؟
+                        </div>
+                    </div>
+                    <div className={"d-flex flex-row gap-2"}>
+                        <Tooltip title={"کاربر"}>
+                            <div className={"company-profile-img align-self-end"}>
+                                <img src={"/img/1.webp"} className={"w-100"}/>
+                            </div>
+                        </Tooltip>
+                        <div className={"chat-item text-justify bg-white rounded-3 py-3 px-3 shadow-sm"}>
+                            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک
+                            است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی
+                            تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی
+                            در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم
+                            افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان
+                            فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و
+                            شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات
+                            پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+                        </div>
+                    </div>
+                    <div className={"d-flex flex-row gap-2"}>
+                        <Tooltip title={"پشتیبانی وبسایت"}>
+                            <div className={"company-profile-img align-self-end"}>
+                                <img alt={""} src={"/img/consulting.svg"} className={"w-100"}/>
+                            </div>
+                        </Tooltip>
+                        <div className={"chat-item text-justify bg-white rounded-3 py-3 px-3 shadow-sm"}>
+                            سلام چطور میتونم کمکتون کنم ؟
+                        </div>
+                    </div>
+                    <div className={"d-flex flex-row gap-2"}>
+                        <Tooltip title={"کاربر"}>
+                            <div className={"company-profile-img align-self-end"}>
+                                <img src={"/img/1.webp"} className={"w-100"}/>
+                            </div>
+                        </Tooltip>
+                        <div className={"chat-item text-justify bg-white rounded-3 py-3 px-3 shadow-sm"}>
+                            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک
+                            است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی
+                            تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی
+                            در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم
+                            افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان
+                            فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و
+                            شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات
+                            پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+                        </div>
+                    </div>
                     <div className={"d-flex flex-row gap-2"}>
                         <Tooltip title={"پشتیبانی وبسایت"}>
                             <div className={"company-profile-img align-self-end"}>
